586a446359cfe0bca0d5267cabf0c4c20e772eab
Villa1220 <andresvillacis1220@gmail.com>
2024-06-24 06:23:13 -0500

    ULTIMOS CAMBIOS

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// Esperar a que el contenido de la página esté cargado
+
 document.addEventListener("DOMContentLoaded", function() {
     const carouselItems = document.querySelectorAll('.carousel-item');
     
-    // Función para manejar el efecto de agrandamiento
     function enlargeImage(index) {
-        // Aplicar la clase 'enlarge' a la imagen en el índice dado
         carouselItems[index].querySelector('img').classList.add('enlarge');
         
-        // Remover la clase 'enlarge' después de 1 segundo
         setTimeout(function() {
             carouselItems[index].querySelector('img').classList.remove('enlarge');
             
-            // Avanzar al siguiente índice
+        
             const nextIndex = (index + 1) % carouselItems.length;
-            enlargeImage(nextIndex); // Llamar recursivamente para el siguiente índice
-        }, 3000); // 1000 milisegundos = 1 segundo
+            enlargeImage(nextIndex); 
+        }, 2000); 
     }
     
-    // Iniciar el efecto de agrandamiento desde el primer índice
+
     enlargeImage(0);
 });
 
-// script.js
-// script.js
 
 
 
