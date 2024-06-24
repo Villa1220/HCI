b869db6f691c5f9befff8759a0ae9f6682b9df8d
Villa1220 <andresvillacis1220@gmail.com>
2024-06-23 19:13:37 -0500

    INDEX

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-document.getElementById("reservasBtn").addEventListener("click", function() {
-    window.location.href = "reservas.html";
+// Esperar a que el contenido de la página esté cargado
+document.addEventListener("DOMContentLoaded", function() {
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    
+    // Función para manejar el efecto de agrandamiento
+    function enlargeImage(index) {
+        // Aplicar la clase 'enlarge' a la imagen en el índice dado
+        carouselItems[index].querySelector('img').classList.add('enlarge');
+        
+        // Remover la clase 'enlarge' después de 1 segundo
+        setTimeout(function() {
+            carouselItems[index].querySelector('img').classList.remove('enlarge');
+            
+            // Avanzar al siguiente índice
+            const nextIndex = (index + 1) % carouselItems.length;
+            enlargeImage(nextIndex); // Llamar recursivamente para el siguiente índice
+        }, 3000); // 1000 milisegundos = 1 segundo
+    }
+    
+    // Iniciar el efecto de agrandamiento desde el primer índice
+    enlargeImage(0);
 });
-
-document.getElementById("miVueloBtn").addEventListener("click", function() {
-    window.location.href = "mivuelo.html";
-});
-
-document.getElementById("iniciarSesionBtn").addEventListener("click", function() {
-    window.location.href = "iniciarsesion.html";
-});
-
-document.querySelector(".menu-btn").addEventListener("click", function() {
-    alert("¡Menú desplegado!");
-});
-
-
